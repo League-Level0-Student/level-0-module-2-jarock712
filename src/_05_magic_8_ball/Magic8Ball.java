9600ec97b11bedad284b4ede9171f3cfac0a4863
SMHS-519-12 <SMHS-519-12@SMHS-519-12.smhsrop.lab>
2020-01-30 16:31:25 -0800

    thank you

--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -1,21 +1,15 @@
-//    Copyright (c) The League of Amazing Programmers 2013-2019
-//    Level 0
-
-
 package _05_magic_8_ball;
-
 import java.util.Random;
-
+import javax.swing.JOptionPane;
 public class Magic8Ball {
-
-	// 1. Make a main method that includes all the steps below….
-
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
-	// 3. Print out this variable
-
-	// 4. Get the user to enter a question for the 8 ball
-
+public static void main(String[] args) {
+	int randomNumber = new Random().nextInt(4);
+	System.out.println(randomNumber);
+	JOptionPane.showInputDialog("Ask any question to the Magic 8 Ball...");
+	if (randomNumber == 0) {
+		JOptionPane.showMessageDialog(null, "Yes");
+	}
+}
 	// 5. If the random number is 0
 
 	// -- tell the user "Yes"
